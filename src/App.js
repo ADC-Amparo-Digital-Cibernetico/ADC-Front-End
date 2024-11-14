896e66f718b90bd288184ef7ad9ee3bfb776b97f
CESAR OLIVEIRA SOUZA <cesar.osouza@senacsp.edu.br>
2024-11-14 17:20:31 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import logo from './logo.svg';
-<<<<<<< HEAD
 import React from 'react';
-import Header from './Componentes/layout/Header/Header';
+import Header from './Componentes/layout/Header/Header.js';
 import Footer from './Componentes/layout/Footer/Footer';
 import HeroSection from './Componentes/layout/HeroSection/HeroSection';
 import ServiceSection from './Componentes/layout/services/ServiceSection';
 import SupportSection from './Componentes/layout/support/SupportSection';
 import WhyADCSection from './Componentes/layout/WhyADC/WhyADCSection';
-=======
+
 import './App.css';
-import Header from './Componentes/header/Header';
->>>>>>> be800780f3dd044d1b759616f16b31255f30fb26
+
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Header />
+      <Header/>
       <HeroSection />
       <ServiceSection />
       <SupportSection />
       <WhyADCSection />
       <Footer />
-=======
-      < Header/>
-  
->>>>>>> be800780f3dd044d1b759616f16b31255f30fb26
+   
     </div>
   );
 }
